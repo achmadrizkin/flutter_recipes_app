956b47856ad7ba44b920e401a96caaf55d517218
achmadrizkin <arizki.nf02@gmail.com>
2021-02-28 12:46:41 +0700

    first commit

---
+++ lib/bottomNavigator.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/ui/page%201/home.dart';
+import 'package:flutter_recipe_app/ui/page%202/addRecipe.dart';
+import 'package:flutter_recipe_app/ui/page%203/userProfile.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+class BottomNavigation extends StatefulWidget {
+  @override
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  PersistentTabController _controller =
+      PersistentTabController(initialIndex: 0);
+  List<Widget> _buildScreens() {
+    return [HomePage(), AddRecipe(), UserProfile()];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.home),
+        title: ("Home"),
+        activeColor: CupertinoColors.activeBlue,
+        inactiveColor: CupertinoColors.black,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.add),
+        title: ("Add Recipe"),
+        activeColor: CupertinoColors.activeBlue,
+        inactiveColor: CupertinoColors.black,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(CupertinoIcons.person),
+        title: ("User Profile"),
+        activeColor: CupertinoColors.activeBlue,
+        inactiveColor: CupertinoColors.black,
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white,
+      handleAndroidBackButtonPress: true,
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears.
+      stateManagement: true,
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style3, // Choose the nav bar style with this property.
+    );
+  }
+}
---
+++ lib/ui/page 2/addRecipe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddRecipe extends StatefulWidget {
+  @override
+  _AddRecipeState createState() => _AddRecipeState();
+}
+
+class _AddRecipeState extends State<AddRecipe> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple,
+    );
+  }
+}
