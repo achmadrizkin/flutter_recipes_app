0fb69e50785d689c85bf6604de674c060d0d87d6
Gunawan <gunawan.arnf@gmail.com>
2021-03-03 14:39:04 +0700

    still need updates..

--- lib/ui/page 3/userProfile.dart
+++ lib/ui/page 3/userProfile.dart
@@ -4,7 +4,71 @@ class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.green,
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: 240.0,
+        child: Stack(
+          children: [
+            ClipPath(
+              clipper: CustomShape(),
+              child: Container(
+                height: 150,
+                color: Colors.green,
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 10), //10
+                    height: 140.0, //140
+                    width: 140.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    "Achmad Rizki",
+                    style: TextStyle(
+                      fontSize: 22, // 22
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 5), //5
+                  Text(
+                    'arizki.nf02@gmail.com',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xFF8492A2),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class CustomShape extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    double height = size.height;
+    double width = size.width;
+    path.lineTo(0, height - 100);
+    path.quadraticBezierTo(width / 2, height, width, height - 100);
+    path.lineTo(width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
--- lib/ui/recipeDetails/recipeDetails.dart
+++ lib/ui/recipeDetails/recipeDetails.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
 
-class RecipeDetails extends StatelessWidget {
+class ProductDetails extends StatefulWidget {
+  @override
+  _ProductDetailsState createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  // for like button
+  bool likeButton;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: RichText(
+          text: TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                  text: 'Recipe',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black,
+                      fontFamily: 'PoppinsBold')),
+              TextSpan(
+                  text: ' Details',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xFF016DF7),
+                      fontFamily: 'PoppinsBold')),
+            ],
+          ),
+        ),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.favorite,
+                  color: likeButton ? Colors.red : Colors.white),
+              onPressed: () {
+                setState(() {
+                  likeButton = !likeButton;
+                });
+
+                if (likeButton) {
+                  //TODO: ADD TO DATABASE
+                }
+
+                if (!likeButton) {
+                  //TODO: DELETE DATABASE
+                }
+              })
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              //TODO: ADD ETC..
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
